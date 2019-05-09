f934955b5ac43adf6f97b958cc53b902f27a23c0
mifsudm <mifsudm@unimelb.edu.au>
2019-05-09 13:17:23 +1000

    ~Auto: Commit

--- Tk/Routing/Route.php
+++ Tk/Routing/Route.php
@@ -1,143 +1,30 @@
 <?php
 namespace Tk\Routing;
 
-use Tk\Collection;
 
 /**
- * Class Route
- *
  * @author Michael Mifsud <info@tropotek.com>
  * @see http://www.tropotek.com/
  * @license Copyright 2016 Michael Mifsud
  * @notes Adapted from Symfony
  * @deprecated This will be removed once the symfony Routing lib is used by all projects
  */
-class Route
+class Route extends \Symfony\Component\Routing\Route
 {
 
     /**
-     * @var string
-     */
-    private $path = '';
-    
-    /**
-     * Can be one of:
-     *  - callable: array
-     *  - callable: anon function
-     *  - object: implementing __invoke method
-     *  - string: class name implementing __invoke method
-     *  - string: function name
-     *  - string: class and method names in the format of `\Namespace\Classname::method`
-     * 
-     * @var callable|string
-     */
-    private $controller = null;
-
-    /**
-     * @var Collection
-     */
-    protected $attributes = null;
-
-    /**
-     * @var array
-     */
-    protected $validMethods = array();
-
-    /**
-     * @var null|CompiledRoute
-     */
-    private $compiled;
-
-
-    /**
-     * construct
-     * Default valid methods are 'GET','POST','HEAD'
+     * Left for compatability
+     * will be deleted once all project routes.php files are updated
      *
      * @param string $path
      * @param object|callable|string $controller A string, callable or object
      * @param array $attributes
      * @param array $validMethods
+     * @deprecated Use: new \Symfony\Component\Routing\Route()
      */
     public function __construct($path, $controller, $attributes = array(), $validMethods = array('GET','POST','HEAD'))
     {
-        $this->path = rtrim($path, '/');
-        if ($this->path == '') $this->path = '/';
-        $this->controller = $controller;
-        $this->attributes = new Collection($attributes);
-        $this->validMethods = $validMethods;
-    }
-
-    /**
-     * Set the valid methods for this route
-     * Valid Methods:
-     *  - 'CONNECT'
-     *  - 'DELETE'
-     *  - 'GET'
-     *  - 'HEAD'
-     *  - 'OPTIONS'
-     *  - 'PATCH'
-     *  - 'POST'
-     *  - 'PUT'
-     *  - 'TRACE'
-     * 
-     * @param array $methods
-     */
-    public function setValidMethods($methods)
-    {
-        $this->validMethods = is_array($methods) ? $methods : array($methods);
-    }
-
-    /**
-     * @return array
-     */
-    public function getValidMethods()
-    {
-        return $this->validMethods;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPath()
-    {
-        return $this->path;
-    }
-    /**
-     * @return string|callable
-     */
-    public function getController()
-    {
-        return $this->controller;
-    }
-    
-    /**
-     * @return Collection
-     */
-    public function getAttributes()
-    {
-        return $this->attributes;
-    }
-    
-
-    /**
-     * Compiles the route.
-     *
-     * @return CompiledRoute A CompiledRoute instance
-     *
-     * @throws \LogicException If the Route cannot be compiled because the
-     *                         path or host pattern is invalid
-     *
-     * @see RouteCompiler which is responsible for the compilation process
-     */
-    public function compile()
-    {
-        if (null !== $this->compiled) {
-            return $this->compiled;
-        }
-        
-        //$class = $this->getOption('compiler_class');
-        //return $this->compiled = $class::compile($this);
-        
-        return $this->compiled = RouteCompiler::compile($this);
+        $defaults = array_merge(array('_controller' => $controller), $attributes);
+        parent::__construct($path, $defaults, [], [], '', [], $validMethods);
     }
 }
--- readme.md
+++ readme.md
@@ -1,38 +1,5 @@
 # tk-routing 
-
-__Project:__ tk-routing  
-__Published:__ 11 May 2016  
-__Web:__ <http://www.domtemplate.com/>  
-__Authors:__ Michael Mifsud <http://www.tropotek.com/>  
   
-A URL Routing lib to support the Tropotek lib V2.
-
-This routing lib has been adapted from the Symfony sources.  
-I wanted the basic functionality of the Symfony routing without the overhead.
-
-## Contents
-
-- [Installation](#installation)
-- [Introduction](#introduction)
-
-
-## Installation
-
-Available on Packagist ([ttek/tk-routing](http://packagist.org/packages/ttek/tk-routing))
-and installable via [Composer](http://getcomposer.org/).
-
-```bash
-composer require ttek/tk-routing
-```
-
-Or add the following to your composer.json file:
-
-```json
-"ttek/tk-routing": "~2.0"
-```
-
-
-## Introduction
-
-
-
+  
+{ Deprecated in favor of  \Symfony\Component\Routing }
+  
